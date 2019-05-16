3fb4de8c98be7039865ae11c87f302e1870cc2a8
dschaffersmith_laptop <dschaffersmith@gmail.com>
2019-05-16 13:44:01 -0400

    Updated runAll.R to Run our Analysis

---
+++ src/runAll.R
@@ -0,0 +1,18 @@
+## This file runs all of the component parts of our project tasks:
+## read_data.R reads in the data
+## wrangle_data.R does some data wrangling
+## plot_data.R plots the data
+## run_analysis.R runs the analysis
+
+## load required packages
+library(dplyr)
+library(readr)
+library(fields)
+library(lme4)
+library(ggplot2)
+library(here)
+
+source('src/read_data.R')
+source('src/wrangle_data.R')
+source('src/plot_data.R')
+source('src/run_analysis.R')
